964679f13f0b98fc3b6901f9b39c1245e88aacab
wqer <1972324624@qq.com>
2016-12-12 10:19:53 +0800

    index.php

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::get('/index', 'HomeController@index');
