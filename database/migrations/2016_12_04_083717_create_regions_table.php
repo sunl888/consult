917f64d6ee8e0633bef75626a8f7a021992849ec
wqer <1972324624@qq.com>
2016-12-12 23:25:44 +0800

    'v2.0'

--- database/migrations/2016_12_04_083717_create_regions_table.php
+++ database/migrations/2016_12_04_083717_create_regions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRegionTable extends Migration
+class CreateRegionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateRegionTable extends Migration
     public function down()
     {
         //
-        Schema::drop('region');
+        Schema::drop('regions');
     }
 }
